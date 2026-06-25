dc66c9d5f3dd0e1338b3bbf6aa8f7c2ab288f54d
shift19684 <Srikomala_Social_Media@outlook.com>
2026-06-25 15:38:59 +0900

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v -k --http1.1 --cert ./crt/certificate.pem.crt --key ./crt/private.pem.key https://cvda-sub-device.cvda.qa-ph3.dconnect2.daihatsu.co.jp/certificate/24pf-jp-qa-ph3-s3-rotate-certificate/20260511-aws-ca.pem
